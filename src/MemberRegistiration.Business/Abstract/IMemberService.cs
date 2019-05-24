cd1c284359a27a2690e1afdf011c351b05e41e41
developerpotato <cafer.talayoglu@gmail.com>
2019-05-24 15:07:03 +0300

    .

---
+++ src/MemberRegistiration.Business/Abstract/IMemberService.cs
@@ -0,0 +1,18 @@
+﻿using MemberRegistiration.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegistiration.Business.Abstract
+{
+    public interface IMemberService
+    {
+        Member Add(Member member);
+        Member Update(Member member);
+        void Delete(Member member);
+        List<Member> GetAll();
+        Member Get(int id);
+    }
+}
